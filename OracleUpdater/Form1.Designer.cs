d521e697af96b001919adf8de682398ef6b74e53
Kaytotes <uutfboomheadshot@users.noreply.github.com>
2015-05-19 23:38:11 +0100

    Initial Commit

---
+++ OracleUpdater/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace OracleUpdater
+{
+    partial class UpdaterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.downloadProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // downloadProgressBar
+            // 
+            this.downloadProgressBar.Location = new System.Drawing.Point(12, 12);
+            this.downloadProgressBar.Name = "downloadProgressBar";
+            this.downloadProgressBar.Size = new System.Drawing.Size(206, 34);
+            this.downloadProgressBar.TabIndex = 0;
+            // 
+            // UpdaterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 58);
+            this.Controls.Add(this.downloadProgressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UpdaterForm";
+            this.Text = "OracleUpdater";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar downloadProgressBar;
+    }
+}
+
---
+++ OracleUpdater/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace OracleUpdater
+{
+    public partial class UpdaterForm : Form
+    {
+        private string downloadServer = "REDACTED";
+
+        public UpdaterForm()
+        {
+            InitializeComponent();
+
+            /* Kill Main Client */
+            Process[] activeProcesses = Process.GetProcesses();
+            foreach( Process currProc in activeProcesses)
+            {
+                string procName = currProc.ProcessName.ToLower();
+                if( procName.CompareTo("oracleclient") == 0)
+                {
+                    currProc.Kill();
+                    break;
+                }
+            }
+
+            // Update OracleNet Client
+            UpdateOracleNet();
+        }
+
+        private void UpdateOracleNet()
+        {
+            //Remove Backup if already exists
+            File.Delete("OracleLauncher_old.exe");
+
+            //Rename old connector - REEBABLE AFTER DEBUG
+            File.Move("OracleLauncher.exe", "OracleLauncher_old.exe");
+
+            //Download Client Update
+            WebClient downloader = new WebClient();
+            //downloader.DownloadDataCompleted += UpdateDownloaded;
+            downloader.DownloadFileCompleted += UpdateDownloaded;
+            downloader.DownloadProgressChanged += UpdateProgressChanged;
+            downloader.DownloadFileAsync(new System.Uri(downloadServer), "OracleLauncher.exe");
+        }
+
+        // Runs as file is downloading
+        void UpdateProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            downloadProgressBar.Value = e.ProgressPercentage;
+        }
+
+        // Runs when file download is complete
+        private void UpdateDownloaded(object sender, AsyncCompletedEventArgs e)
+        {
+            // Reopen OracleClient.exe
+            Process.Start("OracleLauncher.exe");
+
+            MessageBox.Show("Update Completed");
+
+            // Kill Self
+            Application.Exit();
+        }
+    }
+}
